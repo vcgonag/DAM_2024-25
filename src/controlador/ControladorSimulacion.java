8125b611e149694adf817b9b097db153df5ff412
vcgonag@gmail.com <vcgonag@gmail.com>
2025-01-23 02:37:09 +0100

    Second Commit

--- src/controlador/ControladorSimulacion.java
+++ src/controlador/ControladorSimulacion.java
@@ -7,6 +7,8 @@ import vista.Simulador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
@@ -16,104 +18,161 @@ import javax.swing.JOptionPane;
  */
 public class ControladorSimulacion {
 
-	private Simulador simulador;
-	private SimulacionMP simulacionMP;
-	private SimulacionMT simulacionMT;
-	private ActionListener actionListenerSimular;
+    private Simulador simulador;
+    private SimulacionMP simulacionMP;
+    private SimulacionMT simulacionMT;
+    private ActionListener actionListenerSimular;
+    private static List<String> archivosMP = new ArrayList<>();
+    private static List<String> archivosMT = new ArrayList<>();
 
-	/**
-	 * Constructor que inicializa el controlador con las dependencias necesarias.
-	 * 
-	 * @param simulador    la vista de la aplicación.
-	 * @param simulacionMP el modelo para las simulaciones multiproceso.
-	 * @param simulacionMT el modelo para las simulaciones multihilo.
-	 */
-	public ControladorSimulacion(Simulador simulador, SimulacionMP simulacionMP, SimulacionMT simulacionMT) {
-		this.simulador = simulador;
-		this.simulacionMP = simulacionMP;
-		this.simulacionMT = simulacionMT;
-	}
+    /**
+     * Constructor que inicializa el controlador con las dependencias necesarias.
+     *
+     * @param simulador    la vista de la aplicación.
+     * @param simulacionMP el modelo para las simulaciones multiproceso.
+     * @param simulacionMT el modelo para las simulaciones multihilo.
+     */
+    public ControladorSimulacion(Simulador simulador, SimulacionMP simulacionMP, SimulacionMT simulacionMT) {
+        this.simulador = simulador;
+        this.simulacionMP = simulacionMP;
+        this.simulacionMT = simulacionMT;
+    }
 
-	/**
-	 * Método que asocia los eventos a los componentes de la vista. Configura el
-	 * listener del botón de simulación.
-	 */
-	public void control() {
+    /**
+     * Método que asocia los eventos a los componentes de la vista.
+     * Configura el listener del botón de simulación para ejecutar simulaciones.
+     */
+    public void control() {
+        actionListenerSimular = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                String tiempoTotalMP = "";
+                String tiempoTotalMT = "";
+                boolean entradaValida = false;
+                int totalProteinas = 0;
 
-		actionListenerSimular = new ActionListener() {
-			public void actionPerformed(ActionEvent actionEvent) {
-				String tiempoTotalMP = "";
-				String tiempoTotalMT = "";
-				boolean entradaValida = false;
-				int totalProteinas = 0;
+                try {                   
+                    totalProteinas = Integer.parseInt(simulador.getTextFieldNumeroProteinas().getText());
+                    entradaValida = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Error al convertir el número de proteínas.");
+                    JOptionPane.showMessageDialog(null, "Introduzca un número", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                if (entradaValida) {                  
+                    int tipoProteina = selecciontipoProteina();
+                    tiempoTotalMP = simulacionMP.ejecutarSimulacionMultiproceso(tipoProteina, totalProteinas);
+                    tiempoTotalMT = simulacionMT.ejecutarSimulacionMultihilo(tipoProteina, totalProteinas);
 
-				try {
-					// Validar y convertir la entrada
-					totalProteinas = Integer.parseInt(simulador.getTextFieldNumeroProteinas().getText());
-					entradaValida = true;
-				} catch (NumberFormatException e) {
-					System.out.println("Error al convertir el número de proteínas.");
-					JOptionPane.showMessageDialog(null, "Introduzca un numero", "Error", JOptionPane.ERROR_MESSAGE);
-				}
-				if (entradaValida) {
-					// Ejecutar simulaciones si la entrada es válida
-					int tipoProteina = selecciontipoProteina();
-					tiempoTotalMP = simulacionMP.ejecutarSimulacionMultiproceso(tipoProteina, totalProteinas);
-					tiempoTotalMT = simulacionMT.ejecutarSimulacionMultihilo(tipoProteina, totalProteinas);
+                    imprimirPantalla(tiempoTotalMP, tiempoTotalMT, tipoProteina, totalProteinas);
+                }
+            }
+        };
+        simulador.getBtnSimular().addActionListener(actionListenerSimular);
+    }
 
-					imprimirPantalla(tiempoTotalMP, tiempoTotalMT);
-				}
-			}
-		};
-		simulador.getBtnSimular().addActionListener(actionListenerSimular);
-	}
+    /**
+     * Determina el tipo de proteína seleccionado en la vista.
+     *
+     * @return un entero que representa el tipo de proteína seleccionado.
+     */
+    public int selecciontipoProteina() {
+        int tipoProteina = 0;
+        if (simulador.getRadioButton1().isSelected()) {
+            tipoProteina = 1;
+        } else if (simulador.getRadioButton2().isSelected()) {
+            tipoProteina = 2;
+        } else if (simulador.getRadioButton3().isSelected()) {
+            tipoProteina = 3;
+        } else if (simulador.getRadioButton4().isSelected()) {
+            tipoProteina = 4;
+        }
+        return tipoProteina;
+    }
 
-	/**
-	 * Determina el tipo de proteína seleccionado en la vista.
-	 * 
-	 * @return un entero que representa el tipo de proteína seleccionado.
-	 */
-	public int selecciontipoProteina() {
-		int tipoProteina = 0;
-		if (simulador.getRadioButton1().isSelected()) {
-			tipoProteina = 1;
-		} else if (simulador.getRadioButton2().isSelected()) {
-			tipoProteina = 2;
-		} else if (simulador.getRadioButton3().isSelected()) {
-			tipoProteina = 3;
-		} else if (simulador.getRadioButton4().isSelected()) {
-			tipoProteina = 4;
-		}
-		return tipoProteina;
-	}
+    /**
+     * Muestra los resultados de las simulaciones en la vista.
+     * Filtra, ordena y muestra las simulaciones más recientes en el área de texto.
+     *
+     * @param tiempoTotalMP    el tiempo total de la simulación multiproceso.
+     * @param tiempoTotalMT    el tiempo total de la simulación multihilo.
+     * @param tipoProteina     el tipo de proteína seleccionada.
+     * @param totalProteinas   el número de simulaciones realizadas.
+     */
+    public void imprimirPantalla(String tiempoTotalMP, String tiempoTotalMT, int tipoProteina, int totalProteinas) {
+        String URL = ".";
 
-	/**
-	 * Muestra los resultados de las simulaciones en la vista.
-	 * 
-	 * @param tiempoTotalMP el tiempo total de la simulación multiproceso.
-	 * @param tiempoTotalMT el tiempo total de la simulación multihilo.
-	 */
-	public void imprimirPantalla(String tiempoTotalMP, String tiempoTotalMT) {
-		String URL = ".";
+        simulador.getTextAreaSimulacionMP().setText("--------------------------------------------------------------\n");
+        simulador.getTextAreaSimulacionMT().setText("--------------------------------------------------------------\n");
+        File directorio = new File(URL);
+        File[] ficheros = directorio.listFiles();
 
-		simulador.getTextAreaSimulacionMP().setText("--------------------------------------------------------------\n");
-		simulador.getTextAreaSimulacionMT().setText("--------------------------------------------------------------\n");
-		File directorio = new File(URL);
-		File[] ficheros = directorio.listFiles();
-		for (int i = 0; i < ficheros.length; i++) {
-			if (ficheros[i].getName().contains("PROT_MP_")) {
-				simulador.getTextAreaSimulacionMP().append(ficheros[i] + "\n");
-			} else if (ficheros[i].getName().contains("PROT_MT_")) {
-				simulador.getTextAreaSimulacionMT().append(ficheros[i] + "\n");
-			}
+        archivosMP.clear();
+        archivosMT.clear();
 
-		}
-		simulador.getTextAreaSimulacionMP()
-				.append(tiempoTotalMP + "\n--------------------------------------------------------------");
-		simulador.getTextAreaSimulacionMT()
-				.append(tiempoTotalMT + "\n--------------------------------------------------------------");
-		
-		
-	}
+        String filtroProteinaMP = "PROT_MP_" + tipoProteina + "_";
+        String filtroProteinaMT = "PROT_MT_" + tipoProteina + "_";
+        for (File fichero : ficheros) {
+            String nombreArchivo = fichero.getName();
+            if (nombreArchivo.contains(filtroProteinaMP)) {
+                archivosMP.add(nombreArchivo);
+            } else if (nombreArchivo.contains(filtroProteinaMT)) {
+                archivosMT.add(nombreArchivo);
+            }
+        }
 
+        ordenarArchivosPorSimulacion(archivosMP);
+        ordenarArchivosPorSimulacion(archivosMT);
+
+        int inicioMP = Math.max(0, archivosMP.size() - totalProteinas);
+        int inicioMT = Math.max(0, archivosMT.size() - totalProteinas);
+
+        for (int i = inicioMP; i < archivosMP.size(); i++) {
+            simulador.getTextAreaSimulacionMP().append(archivosMP.get(i) + "\n");
+        }
+        simulador.getTextAreaSimulacionMP()
+                .append(tiempoTotalMP + "\n--------------------------------------------------------------");
+
+        for (int i = inicioMT; i < archivosMT.size(); i++) {
+            simulador.getTextAreaSimulacionMT().append(archivosMT.get(i) + "\n");
+        }
+        simulador.getTextAreaSimulacionMT()
+                .append(tiempoTotalMT + "\n--------------------------------------------------------------");
+    }
+
+    /**
+     * Ordena una lista de nombres de archivos según su marca de tiempo.
+     * Las simulaciones más recientes aparecerán al principio.
+     *
+     * @param archivos la lista de nombres de archivos a ordenar.
+     */
+    private void ordenarArchivosPorSimulacion(List<String> archivos) {
+        for (int i = 0; i < archivos.size() - 1; i++) {
+            for (int j = 0; j < archivos.size() - i - 1; j++) {
+ 
+                String timestamp1 = extraerTimestamp(archivos.get(j));
+                String timestamp2 = extraerTimestamp(archivos.get(j + 1));
+
+                if (timestamp1.compareTo(timestamp2) < 0) {
+                    String temp = archivos.get(j);
+                    archivos.set(j, archivos.get(j + 1));
+                    archivos.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    /**
+     * Extrae la marca de tiempo de un nombre de archivo.
+     *
+     * @param nombreArchivo el nombre del archivo del cual extraer la marca de tiempo.
+     * @return la marca de tiempo como cadena.
+     */
+    private String extraerTimestamp(String nombreArchivo) {
+        try {
+            int inicio = nombreArchivo.lastIndexOf("_") - 15; 
+            int fin = nombreArchivo.lastIndexOf("_");
+            return nombreArchivo.substring(inicio, fin);
+        } catch (Exception e) {
+            return "00000000_000000_000"; 
+        }
+    }
 }
